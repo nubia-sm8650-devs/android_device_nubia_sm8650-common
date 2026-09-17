831624ffde2bfcf409fae49c51a6378802ad7162
Michael Bestas <mkbestas@lineageos.org>
2026-09-17 22:06:48 +0300

    sm8650-common: Drop UFFD flag
    Change-Id: I40424ec785b2f1674a5eefe2150bda5c0a170e61

--- common.mk
+++ common.mk
@@ -169,9 +169,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.lineage
 
-# Kernel
-PRODUCT_ENABLE_UFFD_GC := true
-
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
